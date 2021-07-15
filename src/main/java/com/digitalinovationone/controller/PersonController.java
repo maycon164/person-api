0b7c08ba21158ca0f5a96998af65f176bb6400e8
Maycon <mayconfelipe164@gmail.com>
2021-07-14 21:12:37 -0300

    delete by id and verify if exists

--- src/main/java/com/digitalinovationone/controller/PersonController.java
+++ src/main/java/com/digitalinovationone/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,9 @@ public class PersonController {
 		return personService.findById(id);
 	}
 	
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+		personService.delete(id);
+	}
 }
 
